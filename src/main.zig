3390f77a433af2eb1db0a68afead79077b7844ef
lautitux <lauta@duck.com>
2026-01-19 21:20:57 -0300

    feat: read command

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,14 @@ var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
+    var stdin_buffer: [4096]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
     try stdout.print("$ ", .{});
+
+    const command = try stdin.takeDelimiter('\n');
+    if (command) |cmd| {
+        try stdout.print("{s}: command not found\n", .{cmd});
+    }
 }
